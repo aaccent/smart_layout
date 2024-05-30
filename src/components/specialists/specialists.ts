e27430c7cb039641b8959a3cc8607c9309240068
Regina <rmyachina@mail.ru>
2024-05-30 16:58:47 +0300

    feat: specialists

---
+++ src/components/specialists/specialists.ts
@@ -0,0 +1,24 @@
+import Swiper from 'swiper'
+import { Navigation } from 'swiper/modules'
+
+const swiper = new Swiper('.swiper', {
+    modules: [ Navigation ],
+    navigation: {
+      nextEl: '.arrow-btn--next',
+      prevEl: '.arrow-btn--prev',
+    },
+
+    loop: true,
+    slidesPerView: 1.1,
+    spaceBetween: 10,
+    fadeEffect: {
+      crossFade: true,
+    },
+    breakpoints: {
+      1000: {
+        slidesPerView: 4,
+        spaceBetween: 16,
+      },
+    },
+  },
+)
