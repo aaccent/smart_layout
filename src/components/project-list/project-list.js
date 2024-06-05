fe4f11834fb3a9015d26004057497c4ff892acb1
Regina <rmyachina@mail.ru>
2024-06-05 16:03:50 +0300

    fix: pr issues

--- src/components/project-list/project-list.js
+++ src/components/project-list/project-list.js
@@ -1,54 +1,47 @@
 const pluralRule = new Intl.PluralRules('ru-RU')
 const suffixes = new Map([
-  ["one", " проект"],
-  ["two", " проекта"],
-  ["few", " проекта"],
+  [ 'few', 'проекта' ],
+  [ 'many', 'проектов' ],
+  [ 'one', 'проект' ],
+  [ 'other', 'проектов' ],
 ])
 
 function format(n) {
-  const rule = pluralRule.select(n);
-  const suffix = suffixes.get(rule);
-
-  if (suffix) {
-    return `${n}${suffix}`;
-  } else return `${n} проектов`
+  const rule = pluralRule.select(n)
+  return suffixes.get(rule)
 }
 
 void function () {
-  const projectItem = document.querySelectorAll('.project-list__item')
+  const amount = document.querySelector('.project-list .amount')
+  const suffix = document.querySelector('.project-list .suffix')
   const title = document.querySelector('.project-list__title')
 
-  if (!projectItem || !title) return
+  if (!title) return
 
-  const projectsAmount = projectItem.length
-  title.textContent = `найдено ${format(projectsAmount)}`
-} ()
+  const projectsAmountNumber = Number(amount.textContent)
+
+  suffix.textContent = ` ${format(projectsAmountNumber)}`
+
+  let observer = new MutationObserver((mutations) => {
+    const currentAmount = Number(document.querySelector('.project-list .amount').textContent)
+    suffix.textContent = format(currentAmount)
+  })
+
+  observer.observe(title, { childList: true, subtree: true })
+}()
 
 void function () {
-  const allProjectsBtn = document.querySelector('.project-list__filters-button:first-child');
-  const filterBtns = document.querySelectorAll('.project-list__filters-button:not(:first-child)')
-
-  console.log(allProjectsBtn)
-  console.log(filterBtns)
-
-  allProjectsBtn.addEventListener('click', (e) => {
-    e.target.classList.add('_active');
-    console.log(e.target)
-    filterBtns.forEach(el => el.classList.remove('_active'))
-  })
+  const filterBtns = document.querySelectorAll('.project-list__filters-button')
 
   filterBtns.forEach(el => {
     el.addEventListener('click', (e) => {
-      if (e.target.classList.contains('_active')) {
-        e.target.classList.remove('_active');
-        allProjectsBtn.classList.add('_active')
-      } else {
-        document.querySelectorAll('.project-list__filters-button').forEach(el => el.classList.remove('_active'))
-        e.target.classList.add('_active');
+      if (!e.currentTarget.classList.contains('_active')) {
+        document.querySelector('.project-list__filters').querySelector('._active').classList.remove('_active')
+        e.currentTarget.classList.add('_active')
       }
     })
   })
-} ()
+}()
 
 
 
