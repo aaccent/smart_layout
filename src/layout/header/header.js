4dbfbbbb375bfce3455c239d1b6634849e0b2fc3
sevastyan <seva@aaccent.ru>
2024-06-07 16:57:54 +0300

    refactor: remove main menu feature on mobile

--- src/layout/header/header.js
+++ src/layout/header/header.js
@@ -1,5 +1,3 @@
-import { isMobile } from 'globals/adaptive'
-
 void function() {
   const menuBtns = document.querySelectorAll('[data-action="mobile-menu"]')
   const header = document.querySelector('.header')
@@ -14,37 +12,6 @@ void function() {
   menuBtns.forEach(btn => {
     btn.addEventListener('click', toggleMobileMenu)
   })
-}()
-
-void function() {
-  if (!isMobile) return
-
-  const menu = document.querySelector('.header__menu')
-  const mainMenuItems = document.querySelectorAll('.header__menu-item[data-main-menu]')
-
-  const wrapper = document.createElement('li')
-  wrapper.className = 'header__menu-item'
-  wrapper.innerHTML = '<span class="header__menu-item-text">Меню</span>'
-
-  const submenu = document.createElement('div')
-  submenu.className = 'header__submenu'
-
-  const submenuList = document.createElement('ul')
-  submenuList.className = 'header__submenu-list'
-
-  mainMenuItems.forEach(item => {
-    const itemText = item.querySelector('a.header__menu-item-text')
-
-    const li = document.createElement('li')
-    li.innerHTML = `<a href="${itemText.href}">${itemText.textContent}</a>`
-
-    submenuList.append(li)
-    item.remove()
-  })
-
-  submenu.append(submenuList)
-  wrapper.append(submenu)
-  menu.prepend(wrapper)
 }()
 
 void function () {
