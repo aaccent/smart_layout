c7b3a9f0eee830079ff739ab3c6385b98e44c984
Regina <rmyachina@mail.ru>
2024-06-20 15:35:24 +0300

    fix #127

--- src/components/our-clients/our-clients.js
+++ src/components/our-clients/our-clients.js
@@ -1,5 +1,23 @@
 import Swiper from 'swiper'
-import { Navigation, Autoplay} from 'swiper/modules'
+import { Navigation, Autoplay } from 'swiper/modules'
+
+
+function loop() {
+  const slides = Array.from(document.querySelectorAll('.our-clients__item'))
+  const swiperWrapper = document.querySelector('.our-clients .swiper-wrapper')
+
+  const swiperWrapperWidth = swiperWrapper.offsetWidth
+  let slidesWidth = slides.reduce((sum, cur) => sum + cur.offsetWidth, 0)
+
+  if (swiperWrapperWidth < slidesWidth) return
+
+  while (swiperWrapperWidth >= slidesWidth) {
+    slides.forEach(el => {
+      swiperWrapper.append(el.cloneNode(true))
+      slidesWidth += el.offsetWidth
+    })
+  }
+}
 
 new Swiper('.our-clients .swiper', {
     modules: [ Navigation, Autoplay ],
@@ -7,18 +25,22 @@ new Swiper('.our-clients .swiper', {
       nextEl: '.arrow-btn--next',
       prevEl: '.arrow-btn--prev',
     },
-  loop: true,
-
+    loop: true,
     autoplay: {
       delay: 0,
+      disableOnInteraction: false,
     },
     slidesPerView: 'auto',
+    loopAddBlankSlides: false,
     spaceBetween: 5,
     speed: 3000,
     breakpoints: {
       1000: {
         spaceBetween: 8,
       },
+    },
+    on: {
+      beforeInit: loop,
     },
   },
 )
