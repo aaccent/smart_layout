e229d61908e155edb47fbe2567be7fddd76657b3
sevastyan <seva@aaccent.ru>
2024-06-25 14:21:47 +0300

    feat: adaptive for detailed menu catalog in header

--- src/layout/header/header.js
+++ src/layout/header/header.js
@@ -1,3 +1,5 @@
+import { isDesktop, isMobile } from 'globals/adaptive'
+
 void function mobileMenu() {
   const menuBtns = document.querySelectorAll('[data-action="mobile-menu"]')
   const header = document.querySelector('.header')
@@ -27,6 +29,8 @@ void function mobileMenuItems() {
 }()
 
 void function detailMenu() {
+  if (isMobile) return
+
   const detailedItems = document.querySelectorAll('.header__menu-item--detailed')
 
   detailedItems.forEach(item => {
@@ -42,5 +46,23 @@ void function detailMenu() {
       console.log(e)
       document.body.classList.remove('_active-menu')
     })
+  })
+}()
+
+void function detailMenuMobile() {
+  if (isDesktop) return
+
+  const detailedItemsSubmenus = document.querySelectorAll('.header__menu-item--detailed')
+  const logoWrapper = document.querySelector('.header__logo-wrapper')
+
+  detailedItemsSubmenus.forEach((item) => {
+    const submenu = item.querySelector('.header__submenu')
+
+    const clone = logoWrapper.cloneNode(true)
+    clone.querySelector('.header__menu-btn')?.addEventListener('click', () => {
+      item.classList.remove('_active')
+    })
+
+    submenu.prepend(clone)
   })
 }()
