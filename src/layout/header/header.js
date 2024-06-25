d13a3fc8236a87c7541064be3301bd33bdbf885b
sevastyan <seva@aaccent.ru>
2024-06-25 13:51:40 +0300

    feat: detailed menu for catalog categories in header

--- src/layout/header/header.js
+++ src/layout/header/header.js
@@ -1,4 +1,4 @@
-void function() {
+void function mobileMenu() {
   const menuBtns = document.querySelectorAll('[data-action="mobile-menu"]')
   const header = document.querySelector('.header')
   const mobileWrapper = document.querySelector('.header__mobile-wrapper')
@@ -14,7 +14,7 @@ void function() {
   })
 }()
 
-void function () {
+void function mobileMenuItems() {
   const menuItems = document.querySelectorAll('.header__menu-item:has(.header__submenu)')
 
   menuItems.forEach(item => {
@@ -22,6 +22,25 @@ void function () {
 
     text.addEventListener('click', () => {
       item.classList.toggle('_active')
+    })
+  })
+}()
+
+void function detailMenu() {
+  const detailedItems = document.querySelectorAll('.header__menu-item--detailed')
+
+  detailedItems.forEach(item => {
+    const submenu = item.querySelector('.header__submenu')
+
+    item.addEventListener('mouseenter', () => {
+      document.body.classList.add('_active-menu')
+    })
+
+    submenu.addEventListener('mouseleave', (e) => {
+      if (item.contains(e.toElement)) return
+
+      console.log(e)
+      document.body.classList.remove('_active-menu')
     })
   })
 }()
